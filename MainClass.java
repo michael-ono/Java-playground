db4b109529ebb2257a773f77d7b9cdda5b4e4e78
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-21 18:48:22 +0100

    greetings method

--- MainClass.java
+++ MainClass.java
@@ -2,6 +2,11 @@ public class MainClass {
     public static void main(String[] args) {
         String x = "Hello World";
         System.out.println(x);
-        System.out.println("This is fun");
+        System.out.println("This is greetings");
+    }
+
+    public static void greetings() {
+        String name = "Rougekid";
+        System.out.println("Welcome to the java playzone " + name);
     }
 }
