c9a90277648d19b760b23a9d99582c9addb17671
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-21 12:25:43 +0100

    second commit

--- MainClass.java
+++ MainClass.java
@@ -2,5 +2,6 @@ public class MainClass {
     public static void main(String[] args) {
         String x = "Hello World";
         System.out.println(x);
+        System.out.println("This is fun");
     }
 }
