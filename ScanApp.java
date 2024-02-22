b8e70a724aa9b64e089d032bfd0e87f1a41e3586
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-22 09:41:55 +0100

    instance scanner

--- ScanApp.java
+++ ScanApp.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class ScanApp {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+       
+
     }
     
 }
