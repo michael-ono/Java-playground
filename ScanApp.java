29d3c7a0c2db0512d13d6c9708a927853a785bf1
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-22 09:43:15 +0100

    scan close

--- ScanApp.java
+++ ScanApp.java
@@ -5,6 +5,8 @@ public class ScanApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name);
        
 
     }
