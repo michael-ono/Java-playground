0a9884b6981e66bcf9adf396128930f53c894101
Michael Onochie <116271760+michael-ono@users.noreply.github.com>
2024-02-24 16:56:53 +0100

    greetuser method

--- App.java
+++ App.java
@@ -1,9 +1,16 @@
 
-
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world");
+
+        MainFrame myframe = new MainFrame();
+        myframe.init();
+        // greetUser();
     }
 
-}
+    public static void greetUser () {
+        System.out.println("Hello Mike");
+    }
+
+} 
